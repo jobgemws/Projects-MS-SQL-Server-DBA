f1898377ccde46f345c2941162bd441499b56569
Evgeniy Gribkov <37021038+jobgemws@users.noreply.github.com>
2019-01-25 18:24:04 +0300

    Add files via upload

--- SRV/Обновление для мониторинга.sql
+++ SRV/Обновление для мониторинга.sql
@@ -954,3 +954,87 @@ begin
 		EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'ѕоиск перекрывающихс€(лишних) индексов' , @level0type=N'SCHEMA',@level0name=N'srv', @level1type=N'VIEW',@level1name=N'vDelIndexInclude';
 	end
 end
+
+set @str=null;
+set @iscreated=0;
+if(not exists(select top(1) 1 from sys.views where [name]=N'vReadWriteTables' and [schema_id]=SCHEMA_ID(N'inf')))
+begin
+	set @iscreated=1;
+
+	set @str='create view [inf].[vReadWriteTables] as
+	-- „тение/запись таблицы
+	--  учи не рассматриваютс€, у них нет индексов
+	-- “олько те таблицы, к которым обращались после запуска SQL Server
+	
+	SELECT  @@ServerName AS ServerName ,
+	        DB_NAME() AS DBName ,
+			s.name AS SchemaTableName ,
+	        OBJECT_NAME(ddius.object_id) AS TableName ,
+	        SUM(ddius.user_seeks + ddius.user_scans + ddius.user_lookups)
+	                                                               AS  Reads ,
+	        SUM(ddius.user_updates) AS Writes ,
+	        SUM(ddius.user_seeks + ddius.user_scans + ddius.user_lookups
+	            + ddius.user_updates) AS [Reads&Writes] ,
+	        ( SELECT    DATEDIFF(s, create_date, GETDATE()) / 86400.0
+	          FROM      master.sys.databases
+	          WHERE     name = ''tempdb''
+	        ) AS SampleDays ,
+	        ( SELECT    DATEDIFF(s, create_date, GETDATE()) AS SecoundsRunnig
+	          FROM      master.sys.databases
+	          WHERE     name = ''tempdb''
+	        ) AS SampleSeconds
+	FROM    sys.dm_db_index_usage_stats ddius
+			inner join sys.tables as t on t.object_id=ddius.object_id
+			inner join sys.schemas as s on t.schema_id=s.schema_id
+			INNER JOIN sys.indexes i ON ddius.object_id = i.object_id
+	                                     AND i.index_id = ddius.index_id
+	WHERE    OBJECTPROPERTY(ddius.object_id, ''IsUserTable'') = 1
+	        AND ddius.database_id = DB_ID()
+	GROUP BY s.name, OBJECT_NAME(ddius.object_id)
+	--ORDER BY [Reads&Writes] DESC';
+end
+else
+begin
+	set @str='ALTER view [inf].[vReadWriteTables] as
+	-- „тение/запись таблицы
+	--  учи не рассматриваютс€, у них нет индексов
+	-- “олько те таблицы, к которым обращались после запуска SQL Server
+	
+	SELECT  @@ServerName AS ServerName ,
+	        DB_NAME() AS DBName ,
+			s.name AS SchemaTableName ,
+	        OBJECT_NAME(ddius.object_id) AS TableName ,
+	        SUM(ddius.user_seeks + ddius.user_scans + ddius.user_lookups)
+	                                                               AS  Reads ,
+	        SUM(ddius.user_updates) AS Writes ,
+	        SUM(ddius.user_seeks + ddius.user_scans + ddius.user_lookups
+	            + ddius.user_updates) AS [Reads&Writes] ,
+	        ( SELECT    DATEDIFF(s, create_date, GETDATE()) / 86400.0
+	          FROM      master.sys.databases
+	          WHERE     name = ''tempdb''
+	        ) AS SampleDays ,
+	        ( SELECT    DATEDIFF(s, create_date, GETDATE()) AS SecoundsRunnig
+	          FROM      master.sys.databases
+	          WHERE     name = ''tempdb''
+	        ) AS SampleSeconds
+	FROM    sys.dm_db_index_usage_stats ddius
+			inner join sys.tables as t on t.object_id=ddius.object_id
+			inner join sys.schemas as s on t.schema_id=s.schema_id
+			INNER JOIN sys.indexes i ON ddius.object_id = i.object_id
+	                                     AND i.index_id = ddius.index_id
+	WHERE    OBJECTPROPERTY(ddius.object_id, ''IsUserTable'') = 1
+	        AND ddius.database_id = DB_ID()
+	GROUP BY s.name, OBJECT_NAME(ddius.object_id)
+	--ORDER BY [Reads&Writes] DESC';
+end
+
+if(@str is not null)
+begin
+	exec sp_executesql @str;
+
+	if(@iscreated=1)
+	begin
+		EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'ƒанные по чтению/записи таблицы (кучи не рассматриваютс€, т к у них нет индексов).
+“олько те таблицы, к которым обращались после запуска SQL Server' , @level0type=N'SCHEMA',@level0name=N'inf', @level1type=N'VIEW',@level1name=N'vReadWriteTables';
+	end
+end
