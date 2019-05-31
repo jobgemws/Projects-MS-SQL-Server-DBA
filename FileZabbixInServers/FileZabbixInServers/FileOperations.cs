e226c95c08261e0e03148874a52f1373b2ce9a1e
Evgeniy Gribkov <jobgemws@gmail.com>
2019-05-31 17:07:30 +0300

    0

---
+++ FileZabbixInServers/FileZabbixInServers/FileOperations.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileZabbixInServers
+{
+    public static class FileOperations
+    {
+        /// <summary>
+        /// Синхронизация папок и их содержимое в приемнике
+        /// </summary>
+        /// <param name="sourcePath">Путь папки на источнике</param>
+        /// <param name="targetPath">Путь папки на приемнике</param>
+        /// <returns>Количество произведенных изменений
+        /// <para>При удалении на приемнике лишней директории такое изменение считается за единицу</para>
+        /// <para>При копировании на приемник недостающей директории будет считаться сама директория+все ее вложенные элементы как файлы, так и директории</para></returns>
+        public static int SyncElements (string sourcePath, string targetPath)
+        {
+            int res = 0;
+
+            DirectoryInfo sdr = Directory.CreateDirectory(sourcePath);
+            DirectoryInfo tdr = Directory.CreateDirectory(targetPath);
+
+            FileInfo[] sfiles = sdr.GetFiles();
+            FileInfo[] tfiles = tdr.GetFiles();
+
+            string sourceFile;
+            string targetFile;
+
+            var lfiles_st = (from sf in sfiles
+                           join tf in tfiles on sf.Name equals tf.Name into tbl
+                           from t in tbl.DefaultIfEmpty()
+                           where (sf?.Length != t?.Length) || (t is null)
+                             select new { SourceFile = sf.Name}).ToList();
+
+            var lfiles_ts = (from tf in tfiles
+                           join sf in sfiles on tf.Name equals sf.Name into tbl
+                           from t in tbl.DefaultIfEmpty()
+                           where (t == null)
+                           select new { TargetFile = tf.Name }).ToList();
+
+            res = lfiles_st.Count + lfiles_ts.Count;
+
+            for (int i = 0; i < lfiles_st.Count; i++)
+            {
+                sourceFile = sourcePath + $@"\" + lfiles_st[i].SourceFile;
+                targetFile = targetPath + $@"\" + lfiles_st[i].SourceFile;
+                File.Copy(sourceFile, targetFile, true);
+            }
+
+            for (int i = 0; i < lfiles_ts.Count; i++)
+            {
+                targetFile = targetPath + $@"\" + lfiles_ts[i].TargetFile;
+                File.Delete(targetFile);
+            }
+
+            DirectoryInfo[] sdirs = sdr.GetDirectories();
+            DirectoryInfo[] tdirs = tdr.GetDirectories();
+
+            string sourceDirectory;
+            string targetDirectory;
+
+            var ldirs_st = (from sd in sdirs
+                           join td in tdirs on sd.Name equals td.Name into tbl
+                           from t in tbl.DefaultIfEmpty()
+                           //where (t is null)
+                           select new { SourceDirectory = sd.Name }).ToList();
+
+            var ldirs_ts = (from td in tdirs
+                            join sd in sdirs on td.Name equals sd.Name into tbl
+                           from t in tbl.DefaultIfEmpty()
+                           where (t == null)
+                           select new { TargetDirectory = td.Name }).ToList();
+
+            res += ldirs_st.Count + ldirs_ts.Count;
+
+            for (int i = 0; i < ldirs_st.Count; i++)
+            {
+                sourceDirectory = sourcePath + $@"\" + ldirs_st[i].SourceDirectory;
+                targetDirectory = targetPath + $@"\" + ldirs_st[i].SourceDirectory;
+
+                res += SyncElements(sourceDirectory, targetDirectory);
+            }
+
+            for (int i = 0; i < ldirs_ts.Count; i++)
+            {
+                targetDirectory = targetPath + $@"\" + ldirs_ts[i].TargetDirectory;
+                Directory.Delete(targetDirectory, true);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ FileZabbixInServers/FileZabbixInServers/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace FileZabbixInServers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sett = Properties.Settings.Default;
+            string[] servers = sett.Servers.Split(new string[] { ";", "\r", "\n", "\t", " " }, StringSplitOptions.RemoveEmptyEntries);
+            string sourcePath = sett.SourceDirectory;
+            string targetPath = sett.TargetDirectory;
+            string sourceFileConf = sett.SourceFileConf;
+            string targetFileConf = sett.TargetFileConf;
+
+            using (Stream st_log = new FileStream(sett.FileLog, FileMode.Create, FileAccess.Write))
+            {
+                using (TextWriter tw_log = new StreamWriter(st_log, Encoding.UTF8))
+                {
+                    tw_log.WriteLine($"{DateTime.Now} Параметры: Каталог источника: {sourcePath} конфигурационный файл источника: {sourceFileConf}");
+
+                    Sync(tw_log, sourcePath, targetPath, sourceFileConf, targetFileConf, servers);
+
+                    tw_log.WriteLine($"Конец {DateTime.Now}");
+                }
+            }
+
+            Console.WriteLine("Конец");
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Синхронизация настроек
+        /// </summary>
+        /// <param name="tw_log">Файл лога</param>
+        /// <param name="sourcePath">Путь источника</param>
+        /// <param name="targetPath">Путь приемника</param>
+        /// <param name="sourceFileConf">Полное название файла конфигурации источника</param>
+        /// <param name="targetFileConf">Полное название файла конфигурации приемника</param>
+        /// <param name="servers">Коллекция серверов-приемников</param>
+        public static void Sync(TextWriter tw_log, string sourcePath, string targetPath, string sourceFileConf, string targetFileConf, string[] servers)
+        {
+            string server;
+            string targetPath2;
+            string targetFileConf2;
+
+            for (int i = 0; i < servers.Length; i++)
+            {
+                server = servers[i];
+                targetPath2 = $@"\\" + server + $@"\" + targetPath;
+                targetFileConf2 = $@"\\" + server + $@"\" + targetFileConf;
+
+                SyncServer(tw_log, sourcePath, targetPath2, sourceFileConf, targetFileConf2, server);
+            }
+        }
+
+        /// <summary>
+        /// Синхронизация настроек по заданному серверу серверу
+        /// </summary>
+        /// <param name="tw_log">Файл лога</param>
+        /// <param name="sourcePath">Путь источника</param>
+        /// <param name="targetPath">Путь приемника</param>
+        /// <param name="sourceFileConf">Полное название файла конфигурации источника</param>
+        /// <param name="targetFileConf">Полное название файла конфигурации приемника</param>
+        /// <param name="server">Заданный сервер</param>
+        public static void SyncServer(TextWriter tw_log, string sourcePath, string targetPath, string sourceFileConf, string targetFileConf, string server)
+        {
+            int modify = 0;
+
+            tw_log.WriteLine($"{DateTime.Now} Server: {server} начало проверки каталога {targetPath}");
+
+            try
+            {
+                modify = FileOperations.SyncElements(sourcePath, targetPath);
+                tw_log.WriteLine($"{DateTime.Now} Server: {server} успешное окончание проверки каталога {targetPath}");
+
+                if (modify > 0)
+                {
+                    tw_log.WriteLine($"{DateTime.Now} Server: {server} всего произведено изменений не менее {modify}. Старт процесса копирования конфигурационного файла с источника в {targetFileConf}");
+
+                    try
+                    {
+                        File.Copy(sourceFileConf, targetFileConf, true);
+                        tw_log.WriteLine($"{DateTime.Now} Server: {server} успешное окончание процесса копирования конфигурационного файла с источника в {targetFileConf}. Старт процесса перезапуска службы Zabbix Agent");
+
+                        try
+                        {
+                            ServiceController sc = new ServiceController("Zabbix Agent", server);
+                            sc.Stop();
+                            sc.Start();
+
+                            tw_log.WriteLine($"{DateTime.Now} Server: {server} успешное окончание процесса перезапуска службы Zabbix Agent");
+                        }
+                        catch(Exception exp)
+                        {
+                            tw_log.WriteLine($"{DateTime.Now} Server: {server} неуспешное окончание процесса перезапуска службы Zabbix Agent Exception: {exp.Message}");
+                        }
+                    }
+                    catch (Exception exp)
+                    {
+                        tw_log.WriteLine($"{DateTime.Now} Server: {server} неуспешное окончание процесса копирования конфигурационного файла с источника в {targetFileConf} Exception: {exp.Message}");
+                    }
+                }
+                else
+                {
+                    tw_log.WriteLine($"{DateTime.Now} Server: {server} не было выявлено различий в каталоге {targetPath}");
+                }
+            }
+            catch (Exception exp)
+            {
+                tw_log.WriteLine($"{DateTime.Now} Server: {server} неуспешное окончание проверки каталога {targetPath} Exception: {exp.Message}");
+            }
+        }
+    }
+}
